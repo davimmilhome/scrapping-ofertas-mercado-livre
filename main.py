27d63a82b5440dfacb059951dbf782b6724f1c45
Davi Milhome <davimmilhome@gmail.com>
2023-05-22 12:14:33 -0300

    WIP: continuando o main

--- main.py
+++ main.py
@@ -9,9 +9,13 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 
+# Globals
+
+produtos = [] # Lista de produtos
+
 # Instanciando e definindo configs navegador
 options = Options()
-options.add_argument('window-size=400,800') # Seta um tamanho de tela único para evitar dinamicidade
+options.add_argument('window-size=400,800') # Seta um tamanho de tela único
 
 prod = 1
 if prod == 1: # Seta configurações de produção
@@ -21,32 +25,38 @@ navegador = webdriver.Firefox(options=options)
 navegador.get('https://www.mercadolivre.com.br/ofertas')
 sleep(1) # Espera a page carregar
 page_content = navegador.page_source
-site = BeautifulSoup(page_content, 'html.parser') # Convertendo em objeto do bs
+site = BeautifulSoup(page_content, 'html.parser') # Convertendo em bs
 
-#
+# Setando elementos BS
 
-ofertas_div = site.findAll('div', attrs={'class': 'promotion-item__container'})
+ofertas_ol = site.find('ol', attrs={'class': 'items_container'})
+ofertas_li = ofertas_ol.findAll('li', attrs={'class': 'promotion-item sup'})
 
-produtos = []
+for idx, item in enumerate(ofertas_li):
 
-# for idx, ofertas_div in enumerate(ofertas_div):
-#
-#     link_prod = ofertas_div.find('a', attrs={'class': 'promotion-item__link-container'}).text
-#     # nome_prod = o
-#     # vl_antigo_prod =
-#     # vl_atual_prod =
-#     # descont_prod
-#     # link_img_prod
-#     # prazo_frete_prod
-#     # loja_prod =
-#
-#     produto = {
-#         'link_prod' : link_prod,
-#     }
-#     produtos.append(produto)
+    link_prod = item.find(
+        'a', attrs={'class': 'promotion-item__link-container'})['href']
+    nome_prod = item.find(
+        'p', attrs={'class': 'promotion-item__title'}).text
+    vl_antigo_prod = item.find(
+        'span', attrs={'class': 'andes-money-amount__fraction'}).text.replace('.','')
+    # vl_atual_prod =
+    # descont_prod
+    # link_img_prod
+    # prazo_frete_prod
+    # loja_prod =
+
+    produto = {
+        'link_prod' : link_prod,
+        'nome_prod' : nome_prod,
+        'vl_antigo_prod' : vl_antigo_prod,
+    }
+    produtos.append(produto)
 
 
 if __name__ == '__main__':
+    print(produtos)
     #print(site.prettify()) # exibe o html
-    print(ofertas_div)
+    #print(ofertas_ol)
+    #print(ofertas_li)
 
