6ff4c99ee94dc2ba7f3d7ab1a678ea10d3fd56d0
Davi Milhome <davimmilhome@gmail.com>
2023-05-23 07:51:32 -0300

    wip: colocando bloco try

--- main.py
+++ main.py
@@ -22,40 +22,66 @@ if prod == 1: # Seta configurações de produção
     options.add_argument('--headless') # Não exibe o navegador
 
 navegador = webdriver.Firefox(options=options)
-navegador.get('https://www.mercadolivre.com.br/ofertas')
-sleep(1) # Espera a page carregar
-page_content = navegador.page_source
-site = BeautifulSoup(page_content, 'html.parser') # Convertendo em bs
 
-# Setando elementos BS
+"""
+Bloco Try utilizado abaixo para evitar crash, a saida do navegador será
+feita no finally, independete da ocorrencia de erros na execução
+"""
+try:
+    navegador.get('https://www.mercadolivre.com.br/ofertas')
+    sleep(1) # Espera a page carregar
+    page_content = navegador.page_source
+    site = BeautifulSoup(page_content, 'html.parser') # Convertendo em bs
 
-ofertas_ol = site.find('ol', attrs={'class': 'items_container'})
-ofertas_li = ofertas_ol.findAll('li', attrs={'class': 'promotion-item sup'})
+    # Setando elementos BS
 
-for idx, item in enumerate(ofertas_li):
+    ofertas_ol = site.find('ol', attrs={'class': 'items_container'})
+    ofertas_li = ofertas_ol.findAll('li', attrs={'class': 'promotion-item sup'})
 
-    link_prod = item.find(
-        'a', attrs={'class': 'promotion-item__link-container'})['href']
-    nome_prod = item.find(
-        'p', attrs={'class': 'promotion-item__title'}).text
-    vl_antigo_prod = item.find(
-        'span', attrs={'class': 'andes-money-amount__fraction'}).text.replace('.','')
-    # vl_atual_prod =
-    # descont_prod
-    # link_img_prod
-    # prazo_frete_prod
-    # loja_prod =
+    for idx, item in enumerate(ofertas_li):
 
-    produto = {
-        'link_prod' : link_prod,
-        'nome_prod' : nome_prod,
-        'vl_antigo_prod' : vl_antigo_prod,
-    }
-    produtos.append(produto)
+        link_prod = item.find(
+            'a', attrs={'class': 'promotion-item__link-container'})['href']
+
+        nome_prod = item.find(
+            'p', attrs={'class': 'promotion-item__title'}).text
+
+        """
+        O elemento abaixo vem no formato > Antes: 1165 reaisR$1.165 < para 
+        um prod qualquer, sendo assim, precisa do tratamento utilizado
+        """
+        vl_antigo_prod = item.find(
+            's',
+            attrs={
+                'class': 'andes-money-amount andes-money-amount-combo__previous-value andes-money-amount--previous andes-money-amount--cents-comma',}).text
+        vl_antigo_prod = vl_antigo_prod[6:11] # Tratamento
+        vl_antigo_prod = int(vl_antigo_prod)
+
+        vl_atual_prod = item.find(
+            'span', attrs={'class': 'andes-money-amount__fraction'}).text
+
+        vl_atual_prod = int(vl_atual_prod)
+
+        # descont_prod
+        # link_img_prod
+        # prazo_frete_prod
+        # loja_prod =
+
+        produto = {
+            'link_prod' : link_prod,
+            'nome_prod' : nome_prod,
+            'vl_antigo_prod' : vl_antigo_prod,
+            'vl_atual_prod' : vl_atual_prod
+        }
+        produtos.append(produto)
+
+finally:
+    navegador.quit()
 
 
 if __name__ == '__main__':
     print(produtos)
+    print(f' Total de produtos captados: {len(produtos)}')
     #print(site.prettify()) # exibe o html
     #print(ofertas_ol)
     #print(ofertas_li)
