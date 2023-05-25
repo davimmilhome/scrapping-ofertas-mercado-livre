780c8461c7db75581d7af6b1f612f58d2f9a67eb
davimmilhome <davimmilhome@gmail.com>
2023-05-25 13:51:15 -0300

    refactor: adicionando multiprocessamento ao codigo
    Esse commit isola  uma função do programa principal e em seguida aplica
    o método pool.map para o multiprocessamento

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import json
 from time import sleep
-
+from multiprocessing import Pool
 
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
+
 
 def get_site_ML_content(pagina):
     """
@@ -28,9 +29,79 @@ def get_site_ML_content(pagina):
         page_content = navegador.page_source
         site_func = BeautifulSoup(page_content, 'html.parser')  # Objeto BS
         return site_func
-
     finally:
         navegador.quit()
+
+def process_page(pagina):
+    print(f'[LOG]: Página de oferta sendo consultada: {pagina}')
+    site = get_site_ML_content(pagina)
+
+    ofertas_ol = site.find('ol', attrs={'class': 'items_container'})
+    ofertas_li = ofertas_ol.findAll('li')
+
+    produtos = {}
+
+    for idx, item in enumerate(ofertas_li):
+        link_prod = item.find(
+            'a', attrs={'class': 'promotion-item__link-container'})['href']
+        nome_prod = item.find(
+            'p', attrs={'class': 'promotion-item__title'}).text
+
+        vl_antigo_prod = item.find(
+            's', attrs={'class': 'andes-money-amount andes-money-amount'
+            '-combo__previous-value andes-money-amount--previous andes-'
+            'money-amount--cents-comma'}
+        )
+
+        vl_atual_prod_texto = item.find(
+            'span', attrs={'class': 'andes-money-amount__fraction'}).text
+        vl_atual_prod_texto = vl_atual_prod_texto.replace('.', '')
+        vl_atual_prod_centavos = item.find(
+            'span', attrs={'class': 'andes-money-amount__cents andes-money'
+            '-amount__cents--superscript-24'}
+        )
+        if vl_atual_prod_centavos:
+            vl_atual_prod_centavos = vl_atual_prod_centavos.text
+            vl_atual_prod_real = (
+                    vl_atual_prod_texto + '.' + vl_atual_prod_centavos
+            )
+            vl_atual_prod_real = float(vl_atual_prod_real)
+        else:
+            vl_atual_prod_real = float(vl_atual_prod_texto)
+
+        if vl_antigo_prod:
+            vl_antigo_prod = vl_antigo_prod.text
+            vl_antigo_prod = int(
+                vl_antigo_prod[7:vl_antigo_prod.find('reais')].replace('.', '')
+            )
+            descont_prod = 1 - (vl_atual_prod_real / vl_antigo_prod)
+            descont_prod = round(descont_prod, 2)
+        else:
+            descont_prod = None
+
+        loja_prod = item.find(
+            'span', attrs={'class': 'promotion-item__seller'}
+        )
+        if loja_prod:
+            loja_prod = loja_prod.text[4:]
+
+        produto = {
+            'link_prod': link_prod,
+            'nome_prod': nome_prod,
+            'vl_antigo_prod': vl_antigo_prod,
+            'vl_atual_prod_real': vl_atual_prod_real,
+            'descont_prod': descont_prod,
+            'loja_prod': loja_prod,
+            'n_pagina_oferta': pagina,
+        }
+        """
+        armazenar as informações de cada produto
+         individualmente no dicionário, 
+         usando o link do produto como chave.
+        """
+        produtos[link_prod] = produto
+
+    return produtos
 
 def scrapping_mercado_livre_ofertas(paginas=None, producao=False):
     """
@@ -45,8 +116,6 @@ def scrapping_mercado_livre_ofertas(paginas=None, producao=False):
     :return: None
     """
 
-    produtos = []
-
     if producao:
         if paginas:
             n = paginas
@@ -55,73 +124,17 @@ def scrapping_mercado_livre_ofertas(paginas=None, producao=False):
     else:
         n = paginas
 
-    for pagina in range(1, n+1):
-        print(f'[LOG]: Página de oferta sendo consultada: {pagina}')
-        site = get_site_ML_content(pagina)
+    with Pool() as pool:
+        resultados = pool.map(process_page, range(1, n+1))
 
-        ofertas_ol = site.find('ol', attrs={'class': 'items_container'})
-        ofertas_li = ofertas_ol.findAll('li')
+    produtos = {}
+    for resultado in resultados:
+        produtos.update(resultado)
 
-        for idx, item in enumerate(ofertas_li):
-            link_prod = item.find(
-                'a', attrs={'class': 'promotion-item__link-container'})['href']
-            nome_prod = item.find(
-                'p', attrs={'class': 'promotion-item__title'}).text
-
-            vl_antigo_prod = item.find(
-                's', attrs={'class': 'andes-money-amount andes-money-amount'
-                '-combo__previous-value andes-money-amount--previous andes-'
-                'money-amount--cents-comma'}
-            )
-
-            vl_atual_prod_texto = item.find(
-                'span', attrs={'class': 'andes-money-amount__fraction'}).text
-            vl_atual_prod_texto = vl_atual_prod_texto.replace('.', '')
-            vl_atual_prod_centavos = item.find(
-                'span', attrs={'class': 'andes-money-amount__cents andes-money'
-                '-amount__cents--superscript-24'}
-            )
-            if vl_atual_prod_centavos:
-                vl_atual_prod_centavos = vl_atual_prod_centavos.text
-                vl_atual_prod_real = (
-                        vl_atual_prod_texto + '.' + vl_atual_prod_centavos
-                )
-                vl_atual_prod_real = float(vl_atual_prod_real)
-            else:
-                vl_atual_prod_real = float(vl_atual_prod_texto)
-
-            if vl_antigo_prod:
-                vl_antigo_prod = vl_antigo_prod.text
-                vl_antigo_prod = int(
-                    vl_antigo_prod[7:vl_antigo_prod.find('reais')].replace('.', '')
-                )
-                descont_prod = 1 - (vl_atual_prod_real / vl_antigo_prod)
-                descont_prod = round(descont_prod, 2)
-            else:
-                descont_prod = None
-
-            loja_prod = item.find(
-                'span', attrs={'class': 'promotion-item__seller'}
-            )
-            if loja_prod:
-                loja_prod = loja_prod.text[4:]
-
-            produto = {
-                'link_prod': link_prod,
-                'nome_prod': nome_prod,
-                'vl_antigo_prod': vl_antigo_prod,
-                'vl_atual_prod_real': vl_atual_prod_real,
-                'descont_prod': descont_prod,
-                'loja_prod': loja_prod,
-                'n_pagina_oferta': pagina,
-            }
-            produtos.append(produto)
-
-            with open('output/ofertas.json', 'w') as json_file:
-                json.dump(produtos, json_file)
+    with open('output/ofertast.json', 'w') as json_file:
+        json.dump(produtos.values(), json_file)
 
     print(f'Total de produtos captados: {len(produtos)}')
 
 if __name__ == '__main__':
     scrapping_mercado_livre_ofertas(producao=True)
-
