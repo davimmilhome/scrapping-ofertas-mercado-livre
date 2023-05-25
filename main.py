cb4e12ef7cab7c3a3d309981cc88e4979e1928ef
davimmilhome <davimmilhome@gmail.com>
2023-05-25 12:32:26 -0300

    feat: adicionando centavos ao preco principal

--- main.py
+++ main.py
@@ -10,15 +10,16 @@ from selenium.webdriver.firefox.options import Options
 def scrapping_mercado_livre_ofertas(paginas=None, producao=False):
     """
 
-    :param paginas: N° de paginas consultadas
-    :param producao: Se esta em ambiente de produção
+    :param paginas: N° de paginas consultadas. Se não informado, e
+    producao =True, então paginas = 20
+    :param producao: Se esta em producao
     :return:
     """
     # Configurações globais
     produtos = []
     options = Options()
     options.add_argument('window-size=400,800')
-    #options.add_argument('--headless')
+    options.add_argument('--headless')
     if producao:
         if paginas:
             n = paginas
@@ -47,13 +48,25 @@ def scrapping_mercado_livre_ofertas(paginas=None, producao=False):
                 nome_prod = item.find('p', attrs={'class': 'promotion-item__title'}).text
 
                 vl_antigo_prod = item.find('s', attrs={'class': 'andes-money-amount andes-money-amount-combo__previous-value andes-money-amount--previous andes-money-amount--cents-comma'})
-                vl_atual_prod = item.find('span', attrs={'class': 'andes-money-amount__fraction'}).text
-                vl_atual_prod = int(vl_atual_prod.replace('.', ''))
+                vl_atual_prod_texto = item.find(
+                    'span', attrs={'class': 'andes-money-amount__fraction'}).text
+                vl_atual_prod_texto = vl_atual_prod_texto.replace('.', '')
+                vl_atual_prod_centavos = item.find(
+                    'span',
+                    attrs={'class': 'andes-money-amount__cents andes-money-amount__cents--superscript-24'})
+                if vl_atual_prod_centavos:
+                    vl_atual_prod_centavos = vl_atual_prod_centavos.text
+                    vl_atual_prod_real = vl_atual_prod_texto+'.'+ vl_atual_prod_centavos
+                    vl_atual_prod_real = float(vl_atual_prod_real)
+                else:
+                    vl_atual_prod_real = float(vl_atual_prod_texto)
+
+
 
                 if vl_antigo_prod:
                     vl_antigo_prod = vl_antigo_prod.text
                     vl_antigo_prod = int(vl_antigo_prod[7:vl_antigo_prod.find('reais')].replace('.', ''))
-                    descont_prod = 1 - (vl_atual_prod / vl_antigo_prod)
+                    descont_prod = 1 - (vl_atual_prod_real / vl_antigo_prod)
                     descont_prod = round(descont_prod, 2)
                 else:
                     descont_prod = None
@@ -66,7 +79,7 @@ def scrapping_mercado_livre_ofertas(paginas=None, producao=False):
                     'link_prod': link_prod,
                     'nome_prod': nome_prod,
                     'vl_antigo_prod': vl_antigo_prod,
-                    'vl_atual_prod': vl_atual_prod,
+                    'vl_atual_prod_real': vl_atual_prod_real,
                     'descont_prod': descont_prod,
                     'loja_prod': loja_prod,
                     'n_pagina_oferta': pagina,
